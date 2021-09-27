4ff50a4925dc11393f5c1fc9550aed240cd1724d
natalia <nataliacoelho199@gmail.com>
2021-09-27 20:57:43 -0300

    first commit

---
+++ WebCrudBebidas/Controllers/ClientesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebCrudBebidas.Data;
+using WebCrudBebidas.Models;
+
+namespace WebCrudBebidas.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Clientes
+        public async Task<IActionResult> Index(string ordem)
+        {
+            List<Cliente> ListaCLientes = new List<Cliente>();
+            if(string.IsNullOrEmpty(ordem) || ordem == "asc")
+            {
+                ListaCLientes = await _context.Cliente.ToListAsync();
+               ListaCLientes = ListaCLientes?.ToList().OrderBy(x => x.Nome).ToList();
+            } else
+            {
+                ListaCLientes = await _context.Cliente.ToListAsync();
+                ListaCLientes = ListaCLientes?.ToList().OrderByDescending(x => x.Nome).ToList();
+            }
+            return View(await _context.Cliente.ToListAsync());
+
+        }
+
+        // GET: Clientes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Cliente
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // GET: Clientes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome, TipoPessoa, Documento,Telefone,TipoTelefone,Endereco,TipoEndereco")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Cliente.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Clientes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Documento,Telefone,TipoTelefone,Endereco,TipoEndereco")] Cliente cliente)
+        {
+            if (id != cliente.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cliente);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClienteExists(cliente.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Cliente
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return View(cliente);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cliente = await _context.Cliente.FindAsync(id);
+            _context.Cliente.Remove(cliente);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return _context.Cliente.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebCrudBebidas/Models/Cliente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCrudBebidas.Models
+{
+    [Table("Cliente")]
+    public class Cliente
+    {
+        [Column ("Id")]
+        [Display(Name = "Codigo")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("TipoPessoa")]
+        [Display(Name = "Tipo Pessoa")]
+        public string TipoPessoa { get; set; }
+
+        [Column("Documento")]
+        [Display(Name = "CPF/CNPJ")]
+        public string Documento { get; set; }
+
+        [Column("Telefone")]
+        [Display(Name = "Telefone")]
+        public string Telefone { get; set; }
+
+        [Column("TipoTelefone")]
+        [Display(Name = "Tipo de Telefone")]
+        public string TipoTelefone { get; set; }
+        
+        [Column("Endereco")]
+        [Display(Name = "Endereço")]
+        public string Endereco { get; set; }
+                
+        [Column("TipoEndereco")]
+        [Display(Name = "Tipo de Endereço")]
+        public string TipoEndereco { get; set; }
+
+    }
+}
